c41b87b3e96bac3bb6fce0923d957abd6741d631
Dmitri Eryomin <dmitri.eryomin@gmail.com>
2023-12-29 02:33:40 +0300

    feat(app): add contact-us-form component

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './style.css';
 
 import './src/components/logo';
+import { ContactUsForm } from './src/components/contact-us-form';
 
 document.querySelector('.hero .arrows').addEventListener('click', () => {
   document.querySelector('.about-us').scrollIntoView({ behavior: 'smooth' });
@@ -10,47 +11,4 @@ document.querySelector('.about-us .arrows').addEventListener('click', () => {
   document.querySelector('.contact-us').scrollIntoView({ behavior: 'smooth' });
 });
 
-const formLogo = document.querySelector('.form-container main-logo');
-let timeoutId;
-
-function handleBlur() {
-  if (timeoutId) {
-    clearTimeout(timeoutId);
-  }
-  timeoutId = setTimeout(() => {
-    formLogo.setAttribute('animation', 'eyeRoll');
-  }, 800);
-}
-
-function handleFocus() {
-  if (timeoutId) {
-    clearTimeout(timeoutId);
-  }
-  formLogo.removeAttribute('animation');
-}
-
-document.querySelector('[name="fullName"]').addEventListener('focus', () => {
-  handleFocus();
-  formLogo.classList.remove('message');
-  formLogo.classList.remove('email');
-});
-
-document.querySelector('[name="email"]').addEventListener('focus', () => {
-  handleFocus();
-  formLogo.classList.remove('message');
-  formLogo.classList.add('email');
-});
-
-document.querySelector('[name="message"]').addEventListener('focus', () => {
-  handleFocus();
-  formLogo.classList.remove('email');
-  formLogo.classList.add('message');
-});
-
-document
-  .querySelector('[name="fullName"]')
-  .addEventListener('blur', handleBlur);
-
-document.querySelector('[name="email"]').addEventListener('blur', handleBlur);
-
-document.querySelector('[name="message"]').addEventListener('blur', handleBlur);
+const contactUs = new ContactUsForm();
--- src/components/logo.js
+++ src/components/logo.js
@@ -47,7 +47,7 @@ const animations = {
   `,
 };
 
-const svg = ({ w = 20, h = 22, fillColor = 'black', animation }) => `
+const svg = ({ w = 20, h = 22, animation }) => `
 <svg
 width=${w}
 height=${h}
@@ -58,24 +58,24 @@ xmlns="http://www.w3.org/2000/svg"
 ${animation}
 <path
   d="M512.312 325.343V479.085C512.312 563.523 460.344 608.878 386.439 608.878C325.696 608.878 286.164 577.374 274.456 540.833C264.203 572.337 228.353 624.63 147.833 624.63C45.3751 624.63 0 558.463 0 481.603V325.343H512.312ZM36.623 358.725V482.863C36.623 540.833 70.2906 589.349 145.673 589.349C210.803 589.349 255.451 553.426 255.451 464.592V358.725H36.623ZM292.029 467.729C292.029 543.97 324.969 572.956 384.257 572.956C434.747 572.956 475.734 542.711 475.734 480.962V358.725H292.029V467.729Z"
-  fill=${fillColor}
+  fill="var(--logo-color)"
 />
 <path
   d="M256.156 36.8614C377.437 36.8614 475.712 135.86 475.712 257.984H512.312C512.312 115.507 397.646 0 256.156 0C114.688 0 0 115.507 0 257.984H36.6002C36.6229 135.86 134.921 36.8614 256.156 36.8614Z"
-  fill=${fillColor}
+  fill="var(--logo-color)"
 />
 <path
   d="M512.333 258.257H0.0213623V291.661H512.333V258.257Z"
-  fill=${fillColor}
+  fill="var(--logo-color)"
 />
 <path
   d="M142.147 508.091C165.311 508.091 184.089 489.179 184.089 465.85C184.089 442.52 165.311 423.608 142.147 423.608C118.983 423.608 100.205 442.52 100.205 465.85C100.205 489.179 118.983 508.091 142.147 508.091Z"
-  fill=${fillColor}
+  fill="var(--logo-color)"
   id="eye-ball-left"
 />
 <path
   d="M383.87 508.091C407.034 508.091 425.812 489.179 425.812 465.85C425.812 442.52 407.034 423.608 383.87 423.608C360.706 423.608 341.927 442.52 341.927 465.85C341.927 489.179 360.706 508.091 383.87 508.091Z"
-  fill=${fillColor}
+  fill="var(--logo-color)"
   id="eye-ball-right"
 />
 <path
@@ -109,14 +109,9 @@ class Logo extends HTMLElement {
       this.w = parseInt(newValue, 10);
       this.h = this.w * 1.2;
 
-      this.fillColor = getComputedStyle(
-        document.documentElement
-      ).getPropertyValue('--logo-color');
-
       this.shadowRoot.innerHTML = svg({
         w: this.w,
         h: this.h,
-        fillColor: this.fillColor,
         animation: '',
       });
     }
@@ -125,7 +120,6 @@ class Logo extends HTMLElement {
       this.shadowRoot.innerHTML = svg({
         w: this.w,
         h: this.h,
-        fillColor: this.fillColor,
         animation: animations[newValue] || '',
       });
     }
