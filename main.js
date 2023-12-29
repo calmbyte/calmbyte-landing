8209d3a22041fe43c15baf7284c7ed0a57a3feae
Dmitri Eryomin <dmitri.eryomin@gmail.com>
2023-12-29 23:32:44 +0300

    feat(app): add bento grid for about us

--- main.js
+++ main.js
@@ -12,3 +12,24 @@ document.querySelector('.about-us .arrows').addEventListener('click', () => {
 });
 
 const contactUs = new ContactUsForm();
+
+const aboutUsContainer = document.querySelector('.about-us');
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    const [aboutUsObserver] = entries;
+
+    if (aboutUsObserver.isIntersecting) {
+      aboutUsContainer.querySelectorAll('main > div').forEach((node) => {
+        node.classList.add('visible');
+      });
+    }
+  },
+  {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.7,
+  }
+);
+
+observer.observe(aboutUsContainer);
